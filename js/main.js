06b610b0defde2670a4f02899555f37a6fb3c296
Andrew Winchell <awinchell@cobec.com>
2023-11-07 22:19:58 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1731,8 +1731,6 @@ require([
                     $("#save-vertices").css("display", "block");
                     
                     editRouteAttributes();
-
-                    editRoutePath();
                 }
             }
         );
